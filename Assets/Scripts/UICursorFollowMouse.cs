f776285e25967274cc1937e335431ef8732be643
IshaIsha29 <w2065606@westminster.ac.uk>
2025-04-15 16:03:39 +0100

    Initial commit

---
+++ Assets/Scripts/UICursorFollowMouse.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICursorFollowMouse : MonoBehaviour
+{
+    public RectTransform customCursor;
+
+    private void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        customCursor.position = mousePosition;
+    }
+}
